57106d628f2c8323964258bd39612891c8cc1d0d
Philipp Gruber <philipp.gruber@flupps.net>
2024-07-01 20:14:08 +1000

    support 4K screen

--- screen.sh
+++ screen.sh
@@ -10,16 +10,14 @@ XR=`xrandr --display $DISPLAY | grep ' connected' | grep -v eDP-1`
 EXT_SCREEN=`echo $XR | cut -d' ' -f1`
 EXT_RESOLUTION=`echo $XR | cut -d' ' -f3 | sed 's/\+.*$//g'`
 EXT_RESOLUTION_X=`echo $EXT_RESOLUTION | sed 's/x.*$//g'`
+MAX_RESOLUTION=`xrandr --display $DISPLAY | grep $EXT_SCREEN -A5 | grep -v "^$EXT_SCREEN" | sort | tail -n1 | cut -d' ' -f4`
+MAX_RESOLUTION_X=`echo $MAX_RESOLUTION | sed 's/x.*$//g'`
 
 xrandr \
     --display "$DISPLAY" \
-    --output $EXT_SCREEN --mode $EXT_RESOLUTION --pos 0x0 --rotate normal --scale 1x1 \
-    --output eDP-1  --mode 1920x1080 --pos $EXT_RESOLUTION_X''x422 --rotate normal --scale 0.9999x0.9999
+    --output $EXT_SCREEN --mode $MAX_RESOLUTION --pos 0x0 --rotate normal --scale 1x1 \
+    --output eDP-1  --mode 1920x1080 --pos $MAX_RESOLUTION_X''x1422 --rotate normal --scale 0.9999x0.9999
 
-bluetoothctl connect 90:9C:4A:05:DE:59
-
-exit;
-sleep 3
 
 XFCE_PID=$(ps -C xfce4-session -o pid= | sed 's/ //g')
 export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$XFCE_PID/environ)
